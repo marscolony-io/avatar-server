bc431e8c3d8057cd89eb54971330067528b2cf99
crystalbit <crystalbit@mail.ru>
2022-04-02 12:49:59 +0300

    contracts

--- src/helper.ts
+++ src/helper.ts
@@ -1,7 +1,7 @@
 export const avatarImage = (id: number) => 'https://' + (process.env.TESTNET ? 'meta-avatar-test' : 'meta-avatar') + '.marscolony.io/' + id + '.jpg';
 
-export const avatarContract = process.env.TESTNET ? '0xdE165766CC7C48C556c8C20247b322Dd23EB313a' : '';
-export const collectionContract = process.env.TESTNET ? '0xDEfafb07765D9D0F897260BE1389743A09802F20' : '';
+export const avatarContract = process.env.TESTNET ? '0xdE165766CC7C48C556c8C20247b322Dd23EB313a' : '0xCc55065afd013CF06f989448cf724fEC4fF29626';
+export const collectionContract = process.env.TESTNET ? '0xDEfafb07765D9D0F897260BE1389743A09802F20' : '0xFDCC01E0Fe5D3Fb11B922447093EE6862685616c';
 
 export const rpcs = process.env.TESTNET
   ? [
